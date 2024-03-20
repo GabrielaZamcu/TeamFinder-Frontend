e160c0738c6f4f1323f40f75b0cc681f011d8584
GabrielaZamcu <gabriela.zamcu@gmail.com>
2024-03-20 11:28:44 +0200

    New Commit

--- src/pages/statistics/Statistics.jsx
+++ src/pages/statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import StatisticsCSS from "./Statistics.module.css";
 import image2 from "../../assets/Frame86.png";
 import image3 from "../../assets/Frame80.png";
-function AboutUs() {
+function Statistics() {
   return (
     <div className={StatisticsCSS.container}>
       <div className={StatisticsCSS.about_box}>
@@ -13,12 +13,48 @@ function AboutUs() {
           <h2>Statistics</h2>
           <div className={StatisticsCSS.line}></div>
         </div>
-        <div className={StatisticsCSS.statistics}>
-          <img src={image3} alt="Frame80" />
+      </div>
+      <div className={StatisticsCSS.statistics}>
+        <img src={image3} alt="Frame80" />
+      </div>
+      <div className={StatisticsCSS.text}>
+        <text>
+          <b>Organizations over the last year</b>
+        </text>
+        <br></br>
+        <text>
+          finding new employees that match their requests in an innovative way
+        </text>
+        <br></br>
+        <text>
+          <b>Employees over the last year</b>
+        </text>
+        <br></br>
+        <text>
+          Finding project that match their taste and personalities with other
+          employees
+        </text>
+        <br></br>
+        <text>our application was created to</text>
+        <br></br>
+        <text>
+          {" "}
+          keep you working on what you enjoy the most and also to help you get
+          out of the comfort zone and succed in your new projects
+        </text>
+        <div className={StatisticsCSS.redline}>
+          <text>
+            <b>-</b>
+          </text>
+        </div>
+        <div className={StatisticsCSS.blackline}>
+          <text>
+            <b>-</b>
+          </text>
         </div>
       </div>
     </div>
   );
 }
 
-export default AboutUs;
+export default Statistics;
